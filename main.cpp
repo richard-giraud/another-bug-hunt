212310a45099345b66d98d7fde78db37affc1529
Richard Giraud <richard@rgent.ca>
2023-02-19 09:09:33 -0700

    Opens window on start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <stdexcept>
+#include <SDL2/SDL.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Window* window;
+    bool gameIsRunning = false;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        throw std::runtime_error("Could not initialize SDL");
+    }
+
+    window = SDL_CreateWindow("Another Bug Hunt", 0, 0, 640, 480, 0);
+
+    if(window == nullptr) {
+        throw std::runtime_error("Could not create the window");
+    }
+
+    // Keep running until the window is closed
+    gameIsRunning = true;
+    while (gameIsRunning) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_WINDOWEVENT) {
+                if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+                    gameIsRunning = false;
+                }
+            }
+        }
+    }
+
     return 0;
 }
