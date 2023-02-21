28fa5ac2802d40176191dd5db732b9603964425b
Richard Giraud <richard@rgent.ca>
2023-02-20 19:43:27 -0700

    Refactor: Changed Room colors to make it easier to see; changed how Room is drawn

--- src/models/Room.cpp
+++ src/models/Room.cpp
@@ -7,18 +7,15 @@
 Room::Room(int x, int y, int w, int h) : positionX{x}, positionY{y}, width{w}, height{h} {}
 
 void Room::render(cairo_t *cr) const {
-    cairo_set_source_rgba(cr, 1, 0, 0, 1);
+    cairo_set_source_rgba(cr, 0.75, 0.75, 0.75, 1);
+    cairo_set_line_width(cr, 4);
     cairo_rectangle(
             cr,
             positionX, positionY,
             width, height
     );
-    cairo_fill(cr);
-    cairo_set_source_rgba(cr, 0, 0, 0.5, 1);
-    cairo_rectangle(
-            cr,
-            positionX + 2, positionY + 2,
-            width - 4, height - 4
-    );
+    cairo_stroke_preserve(cr);
+
+    cairo_set_source_rgba(cr, 0, 0, 0, 1);
     cairo_fill(cr);
 }
